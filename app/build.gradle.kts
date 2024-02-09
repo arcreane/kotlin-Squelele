93344684107b95a96851b03eda130c8fed643553
Squelele <elerig.gassien@outlook.fr>
2024-02-09 10:42:28 +0100

    correction of dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 }
--- app/src/main/java/com/example/mymastermind/HintsScreen.kt
+++ app/src/main/java/com/example/mymastermind/HintsScreen.kt
@@ -6,11 +6,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mymastermind.model.GameViewModel
 
 @Composable
-fun HintsScreen(gameViewModel: GameViewModel = viewModel()) {
+fun HintsScreen(gameViewModel: GameViewModel = GameViewModel()) {
     val gameState = gameViewModel.gameState.collectAsState().value
 
     Column() {
